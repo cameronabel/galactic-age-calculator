0cd799a264d4b44fa04d40af4e95867ee139bcab
Cameron Abel <cameronabel@gmail.com>
2022-12-21 19:51:34 -0800

    ages Map Venus key test passed

--- src/person.js
+++ src/person.js
@@ -2,6 +2,7 @@ export default class Person {
   constructor (age) {
     this.ages = new Map([
       ['Mercury', Math.round(age / 0.24)],
+      ['Venus', Math.round(age / 0.62)],
       ['Earth', age]
     ]);
   }
